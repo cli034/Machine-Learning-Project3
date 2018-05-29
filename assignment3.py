78617757a2870da40c5eb58e350f742b6896f5b3
Adrian Li <adrian@Adrians-MacBook-Pro.local>
2018-05-28 18:45:43 -0700

    knee plot

--- assignment3.py
+++ assignment3.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Question 0
+#load in dataset
 def loadData(fileName):
     #we don't want the labels
     irisData = np.loadtxt(fileName, delimiter=',', usecols=(0, 1, 2, 3))
 
     return irisData
 
+#Question 1
 # Lp norm distance formula
 # From assignment 1
 # p = 2 in this assignment because we want Euclidean distance
@@ -24,7 +27,12 @@ def distance(x,y,p):
     distance = total ** (float(1)/p)
     return distance
 
+#Question 1
+#find the sum of square errors
+#perform the formula on each data point for each feature
 def sum_of_square_error(dataSet, clusters, k, init_centroids):
+    all_error_total = 0.0
+    sum_of_square_error_list = []
     for i in range(0, k):
         temp = []
         for j in range(0, dataSet.shape[0]):
@@ -33,19 +41,27 @@ def sum_of_square_error(dataSet, clusters, k, init_centroids):
         cluster_group = np.array(temp)
     
         number = 0.0
-        sum_of_square_error_list = []
+        
         total = 0.0
         for p in range(0, cluster_group.shape[0]):
             for k in range(0, cluster_group.shape[1]):
                 number = (cluster_group[p][k] - init_centroids[i][k]) ** 2
                 total = total + number
         sum_of_square_error_list.append(total)
-        sum_of_square_error_array = np.array(sum_of_square_error_list)
-        #print total
-        print sum_of_square_error_array
-    #return total          
+                
+        all_error_total = all_error_total + total
+
+    sum_of_square_error_array = np.array(sum_of_square_error_list)
+    print sum_of_square_error_array
+    standDeviation = np.std(sum_of_square_error_array)
+    print "Combined total: " + str(all_error_total)
+    print "Standard Deviation: " + str(standDeviation) + "\n" 
+    #print all_error_total
+    return (all_error_total, standDeviation)
+      
 
 
+#Question 1
 #k is the number of clusters
 def chooseCentroids(dataSet, k):
     init_centroids_List = []
@@ -62,6 +78,8 @@ def chooseCentroids(dataSet, k):
     # print init_centroids
     return init_centroids
 
+#Question 1
+#to choose a set of new centroids by taking the mean of the cluster set
 def reevaluateCenters(dataSet, clusters, k):
     all_new_centers_list = []
     for i in range(0, k):
@@ -83,8 +101,9 @@ def reevaluateCenters(dataSet, clusters, k):
     all_new_center = np.array(all_new_centers_list)
     #print all_new_center
     return all_new_center
-    
 
+#Question 1   
+# k means algorithm
 #x_input is the dataset
 #k is the number of clusters
 #init_centroids
@@ -113,6 +132,44 @@ def k_means_clustering(x_input, k, init_centroids):
     updating_centroids_array = reevaluateCenters(x_input, cluster_assignments, k)
     return (cluster_assignments, updating_centroids_array)
 
+def knee_plot():
+    print "For k = 1 to 10\n"
+    k_clusters_array = [1,2,3,4,5,6,7,8,9,10]
+    SSE_array = []
+    SD_array = []
+
+    for i in k_clusters_array:
+        knee_counter = 1
+        knee_current_centroids = chooseCentroids(irisData, i)
+        knee_init_centroids = knee_current_centroids
+
+        (knee_cluster_assignment, knee_updating_centroids) = k_means_clustering(irisData, i, knee_init_centroids)
+        knee_current_centroids = knee_updating_centroids
+
+        #print ("Sum of square error: ")
+        # sum of square error after the algorithm runs once
+        #sum_of_square_error(irisData, cluster_assignment, i, updating_centroids)
+
+        #runs the k means algorithm until the centroid doesn't change anymore
+        while (1):
+            (knee_cluster_assignment, knee_updating_centroids) = k_means_clustering(irisData, i, knee_current_centroids)
+            if (np.array_equal(knee_updating_centroids, knee_current_centroids)):
+                knee_final_centroids = knee_updating_centroids
+                (error_total, standardDev) = (sum_of_square_error(irisData, knee_cluster_assignment, i, knee_final_centroids))
+                SSE_array.append(error_total)
+                SD_array.append(standardDev)
+                break
+            knee_current_centroids = knee_updating_centroids
+            #print total_error
+            knee_counter = knee_counter + 1
+        
+        print "Number of iterations: " + str(knee_counter) + "\n"
+
+    plt.title("Knee Plot: SSE x Number of clusters")
+    plt.ylabel("Sum of Squared Errors")
+    plt.xlabel("k Clusters value")
+    plt.errorbar(k_clusters_array, SSE_array, yerr=SD_array, fmt='-o')
+    plt.show()
         
 
 # here is the main
@@ -120,21 +177,33 @@ irisData = loadData('iris-data.txt')
 
 num_clusters = input("How many k-clusters: ")
 
+iterationCounter = 1
+
 current_centroids = chooseCentroids(irisData, num_clusters)
 init_centroids = current_centroids
 
 (cluster_assignment, updating_centroids) = k_means_clustering(irisData, num_clusters, init_centroids)
-#this is bad implementation
 current_centroids = updating_centroids
-print ("Sum of square error: ")
-sum_of_square_error(irisData, cluster_assignment, num_clusters, updating_centroids)
-print ("\n")
 
+print ("Sum of square error: ")
+# sum of square error after the algorithm runs once
+#sum_of_square_error(irisData, cluster_assignment, num_clusters, updating_centroids)
+
+#runs the k means algorithm until the centroid doesn't change anymore
 while (1):
     (cluster_assignment, updating_centroids) = k_means_clustering(irisData, num_clusters, current_centroids)
     if (np.array_equal(updating_centroids, current_centroids)):
         final_centroids = updating_centroids
+        sum_of_square_error(irisData, cluster_assignment, num_clusters, final_centroids)
         break
     current_centroids = updating_centroids
+    #print total_error
+    iterationCounter = iterationCounter + 1
 
-sum_of_square_error(irisData, cluster_assignment, num_clusters, final_centroids)
+# sum of square error for the final centroids
+#sum_of_square_error(irisData, cluster_assignment, num_clusters, final_centroids)
+print "Number of iterations: " + str(iterationCounter) + "\n"
+
+#--------------------------------------------------------------------------------------------------------------------------
+
+knee_plot()
